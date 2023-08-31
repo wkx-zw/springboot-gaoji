e78bb093f6ad719da3e7345ca01ebd22f89ccc32
朱文 <1549934465@qq.com>
2023-08-31 13:52:09 +0800

    hot-fix commit

--- springboot-vue/src/main/java/com/cs/springbootvue/GitTest.java
+++ springboot-vue/src/main/java/com/cs/springbootvue/GitTest.java
@@ -12,5 +12,6 @@ package com.cs.springbootvue;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("hot-fix");
     }
 }

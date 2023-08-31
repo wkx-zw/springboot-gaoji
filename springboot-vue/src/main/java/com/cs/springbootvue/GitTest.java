d41b3973669e69ffa673ab6540733f30e4095b7e
朱文 <1549934465@qq.com>
2023-08-31 14:32:30 +0800

    host-test commit2

--- springboot-vue/src/main/java/com/cs/springbootvue/GitTest.java
+++ springboot-vue/src/main/java/com/cs/springbootvue/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("hot-fix");
+        System.out.println("hot-tset");
+
     }
 }

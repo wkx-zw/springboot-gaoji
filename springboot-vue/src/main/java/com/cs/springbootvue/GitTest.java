a0037db286387e2ff92c806560d0f49c6017a439
朱文 <1549934465@qq.com>
2023-08-31 14:32:01 +0800

    master-test commit

--- springboot-vue/src/main/java/com/cs/springbootvue/GitTest.java
+++ springboot-vue/src/main/java/com/cs/springbootvue/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("hot-fix");
+        System.out.println("mater-tset");
     }
 }
